f757dcfe852a66929cd8b994cf3799706ca0af9d
Chyen <239882848@qq.com>
2022-04-17 19:41:51 +0800

    Initial Commit

---
+++ C++Lab8/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  C++Lab8
+//
+//  Created by CHYEN on 2022/4/17.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
